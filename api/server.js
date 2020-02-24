3cabad5144329b48e1e64771f6b25bfcb0b23263
Laura <theimer.laura@gmail.com>
2020-02-23 21:10:19 -0500

    inital set up.

--- api/server.js
+++ api/server.js
@@ -15,4 +15,31 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+// testing server 
+// 
+server.get('/', (req, res) => {
+    res.send(`<h1> Spring Challenge Authentication! 👸`)
+});
+
+
+// testing token 
+// 
+server.get('/token', (req, res) => {
+    const payload= {
+        subject: 'testertoken',
+        username: 'tessthetester',
+        favoriteColor:'purple'
+    };
+
+    const secret = "don't test tess";
+
+    const options = {
+        expiresIn: '8h'
+    };
+
+    const token = jwt.sign(payload, secret, options);
+
+    res.json(token);
+});
+
 module.exports = server;
